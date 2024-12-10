486df010c29104cfa64ba6ff336051bc849f9d7e
Denis Shulika <denisshulika31@gmail.com>
2024-12-10 21:07:48 +0200

    Reorganized file structure and removed unnecessary code.

---
+++ app/src/main/kotlin/com/denisshulika/road_radar/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.denisshulika.road_radar
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
