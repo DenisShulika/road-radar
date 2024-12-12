882fe09e87f88eb1cdd9d9080e7228c02b6d4c5c
Denis Shulika <denisshulika31@gmail.com>
2024-12-12 18:35:54 +0200

    Add layout folder and activity_main.xml inside the layout directory.

--- app/src/main/kotlin/com/denisshulika/road_radar/MainActivity.kt
+++ app/src/main/kotlin/com/denisshulika/road_radar/MainActivity.kt
@@ -2,10 +2,11 @@ package com.denisshulika.road_radar
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import android.widget.Button
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContentView(R.layout.activity_main)
     }
 }
