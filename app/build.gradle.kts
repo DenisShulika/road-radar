edcbb2ea2de48e4def4c521586291ec4371e0a8f
Denis Shulika <denisshulika31@gmail.com>
2024-12-18 00:28:06 +0200

    Refactor: Migrate from XML to Jetpack Compose for UI. Also added ui/theme/Color.kt, Theme.kt, and Typo.kt files for future use in Jetpack Compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.denisshulika.road_radar"
-        minSdk = 30
+        minSdk = 29
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
@@ -27,28 +27,32 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
     }
+
     buildFeatures {
         compose = true
     }
 }
 
 dependencies {
+    implementation(libs.ui)
+    implementation(libs.material3)
+    implementation(libs.ui.tooling.preview)
+    implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -63,10 +67,9 @@ dependencies {
     implementation(libs.hilt.android)
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
-    implementation(libs.glide)
+    implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.lifecycle.livedata.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
-    implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.work.runtime.ktx)
     debugImplementation(libs.leakcanary.android)
     testImplementation(libs.mockito.core)
--- app/src/main/kotlin/com/denisshulika/road_radar/MainActivity.kt
+++ app/src/main/kotlin/com/denisshulika/road_radar/MainActivity.kt
@@ -1,33 +1,29 @@
 package com.denisshulika.road_radar
 
 import android.os.Bundle
-import android.view.View
 import androidx.activity.ComponentActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        // Встановлюємо Edge-to-Edge режим
-        WindowCompat.setDecorFitsSystemWindows(window, false)
-
-        // Враховуємо Insets для контейнера
-        val mainContainer = findViewById<View>(R.id.main_container)
-        ViewCompat.setOnApplyWindowInsetsListener(mainContainer) { view, insets ->
-            val systemBarsInsets = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            view.setPadding(
-                view.paddingLeft,
-                systemBarsInsets.top, // Відступ для статус-бару
-                view.paddingRight,
-                systemBarsInsets.bottom // Відступ для навігаційної панелі
-            )
-            insets
+        enableEdgeToEdge()
+        setContent {
+            Screen()
         }
-
-
     }
+}
+
+@Composable
+fun Screen(){
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Screen()
 }
