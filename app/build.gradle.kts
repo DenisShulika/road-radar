6569721eb1f4eeb2001c8a4ff50c89b9403b6733
Denis Shulika <denisshulika31@gmail.com>
2024-12-22 13:52:27 +0200

    Changed minSdk to 24 to cover more users

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.denisshulika.road_radar"
-        minSdk = 29
+        minSdk = 24
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
