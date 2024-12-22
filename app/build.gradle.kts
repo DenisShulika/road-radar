f004da8f7b281dff522a86703b64543080fa192c
Denis Shulika <denisshulika31@gmail.com>
2024-12-22 14:15:30 +0200

    Added project to Firebase and added Firebase to project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
 android {
@@ -76,4 +77,6 @@ dependencies {
     implementation(libs.accompanist.navigation.material)
     implementation(libs.accompanist.systemuicontroller)
     implementation(libs.timber)
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics)
 }
